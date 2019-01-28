84aa8e811df19dc8fdbeedeff4efe8768cf7e8c6
thunderInfy <r.aditya0824@gmail.com>
2019-01-28 07:25:52 +0530

    modified shell.cpp, added comments

--- Assignment 2 files/shell.cpp
+++ Assignment 2 files/shell.cpp
@@ -19,7 +19,8 @@ Name : Aditya Rastogi (16CS30042)
 #define EXIT_FLAG 0
 using namespace std;
 
-int breakString(char **list,char *str,const char *delim){	// break a string according to a delimiter
+// break a string according to a delimiter
+int breakString(char **list,char *str,const char *delim){
 	int i=0;
 	list[i]=strtok(str,delim);
 	while(list[i]!=NULL){
@@ -29,63 +30,75 @@ int breakString(char **list,char *str,const char *delim){	// break a string acco
 	return i;
 }
 
-int findInput(char *input){				// find the index of <
-	for(int i=0;i<strlen(input);i++){
+// find the index of <
+int findInput(char *input){
+	for(int i=0;input[i]!='\0';i++){
 		if(input[i]=='<')
 			return i;
 	}
 	return -1;
 }
 
-int findOutput(char *input){			// find the index of >
-	for(int i=0;i<strlen(input);i++){
+// find the index of >
+int findOutput(char *input){
+	for(int i=0;input[i]!='\0';i++){
 		if(input[i]=='>')
 			return i;
 	}
 	return -1;
 }
 
-void executeExternal(char *input){		// execute external command
+// execute external command
+void executeExternal(char *input){
 
 	char *args[MAX];
-	if(!strcmp(input,"exit")){		//If string is quit then quit
+
+	//If string is exit then exit
+	if(!strcmp(input,"exit")){		
 		printf("\nExiting from kgp-shell\n");
 		exit(0);
 	}
 
 	breakString(args,input," \n");
 
-	execvp(args[0],args);			//execute the  other program
+	//execute the  other program
+	execvp(args[0],args);
 	printf("Error in executing the file! \n");
 	kill(getpid(),SIGTERM);
 
 }
 
 void executeInputOutput(char *input){
+
 	char *args[MAX];
 	char *files[MAX];
+
 	int i=0;
 
+	//in is the index of < character
 	int in=findInput(input);
-	int out=findOutput(input);
-	// printf("%d %d ",in,out);
-	i=breakString(args,input,"&<>\n");
-	// printf(" %d ",i);
-	// printf("%s %s ",args[1],args[0]);
 
-	// pid_t p=fork();
+	//out is the index of > character
+	int out=findOutput(input);
+	
+	// break input according &, <, > and \n and get size of args in i
+	i=breakString(args,input,"&<>\n");
+	
 	pid_t p=0;
+	
 	if(p==0){
 		if(in==-1 && out==-1){
 			if(i<=1){
 				executeExternal(args[0]);
 			}
-			else printf("Error encountered!\n");
+			else{ 
+				printf("Error encountered! Unexpected additional parameters\n");
+			}
 		}
 		else if(in>0 && out==-1){
 			if(i==2){
 				int f=breakString(files,args[1]," \n");
-				// printf("%s %s %d\n",files[0],files[1],f);
+
 				if(f!=1){
 					printf("Input file error.");
 					return;
@@ -95,6 +108,8 @@ void executeInputOutput(char *input){
 					printf("Error when opening input file.\n");
 					return;
 				}
+
+				//redirect STDIN to the mentioned input file
 				close(0);
 				dup(ifd);
 				close(ifd);
@@ -113,6 +128,8 @@ void executeInputOutput(char *input){
 					printf("Error when opening output file.\n");
 					return;
 				}
+
+				//redirect STDOUT to the mentioned output file
 				close(1);
 				dup(ofd);
 				close(ofd);
@@ -120,7 +137,6 @@ void executeInputOutput(char *input){
 		}
 		else if(in>0 && out>0 && i==3){
 			int ifd,ofd,f;
-			// printf("in out identified ");
 			if(in>out){
 				f=breakString(files,args[1]," \n");
 				if(f!=1){
@@ -142,7 +158,6 @@ void executeInputOutput(char *input){
 					printf("Error in output file\n");
 					return;
 				}
-				// printf("%s ",files[0]);
 				ofd = open(files[0], O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
 
 				f=breakString(files,args[1]," \n");
@@ -150,9 +165,11 @@ void executeInputOutput(char *input){
 					printf("Error in output file\n");
 					return;
 				}
-				// printf("%s ",files[0]);
 				ifd = open(files[0], O_RDONLY);				
 			}
+
+			//redirect STDIN and STDOUT to mentioned files
+
 			close(0);
 			dup(ifd);
 			close(1);
@@ -168,6 +185,7 @@ void executeInputOutput(char *input){
 }
 
 void executePipe(char** args, int N){
+
 	//define N-1 pipes
 	int p[N-1][2];
 
@@ -182,25 +200,35 @@ void executePipe(char** args, int N){
 	//create N child processes
 	pid_t pd;
 	for(int i=0;i<N;i++) { 
-		// printf("%d",i);
         if((pd=fork()) == 0) { 
         	//this is the child process
         	//read from the previous pipe if it exists
         	if(i!=0){
-        		//redirects STDIN to read end of the previous pipe
+        		
+        		//it doesn't write to the previous pipe
 				close(p[i-1][1]);
+
+        		//redirect STDIN to read end of the previous pipe
         		dup2(p[i-1][0], 0);
+				
+				//close the read pipe end
 				close(p[i-1][0]); 
         	}
         	
         	//write to the next pipe if it exists
         	if(i!=N-1){
-        		//redirects STDOUT to write end of the next pipe
+        		
+        		//it doesn't read from the next pipe
 				close(p[i][0]);
+        		
+				//redirects STDOUT to write end of the next pipe
         		dup2(p[i][1], 1);
+
+        		//close the write pipe end
 				close(p[i][1]);	
         	}
-			for(int j=0;j<N-1;j++){	// closes all pipes
+			for(int j=0;j<N-1;j++){	
+			// closes all pipes
 				close(p[j][0]);
 				close(p[j][1]);
 			}
@@ -212,7 +240,9 @@ void executePipe(char** args, int N){
             exit(0); 
         }
 		else {
+			//wait for a while
 			usleep(10000);
+
 			for(int j=0;j<i;j++){	// closes all pipes
 				close(p[j][0]);
 				close(p[j][1]);
@@ -220,7 +250,6 @@ void executePipe(char** args, int N){
 		}
     }
 	while(wait(NULL)>0);
-	// waitpid(-1,NULL,0);
 	exit(0);
 }
 
@@ -231,8 +260,8 @@ int main(int argc, char *argv[]){
 
 	while(1){
 
+		//strings for I/O and for holding other data
 		char pwd[LEN]="";
-		char command[LEN]="";
 		char input[LEN]="";
 		char *ptr;
 
@@ -241,10 +270,9 @@ int main(int argc, char *argv[]){
 
 		//error in getcwd
 		if(ptr==NULL) {
-			perror("getcwd error");
+			perror("Error in getting the current directory");
 			continue;
 		}
-		//printf("%s$ ", pwd);
 
 		//print present working directory in blue colour
 		cout<<"\033[1;34m"<<pwd<<"\033[0m"<<"$ ";
@@ -252,6 +280,7 @@ int main(int argc, char *argv[]){
 		//get command from user
 		fgets(input, 1000, stdin);	
 
+		//check for exit condition
 		if(strlen(input)>=4){
 			if(input[0]=='e' && input[1]=='x' && input[2]=='i' && input[3]=='t'){
 				int k = 4;
@@ -265,9 +294,11 @@ int main(int argc, char *argv[]){
 			}
 		}
 
+		//flag to check for & in the command
 		int backFlag = 0;
-
 		int j;
+
+		//checking & character followed by any number of spaces
 		for(int k=0;input[k]!='\0';k++){
 			if(input[k]=='&'){
 				j = k;
@@ -297,18 +328,12 @@ int main(int argc, char *argv[]){
 			args[i]=strtok(NULL,"|");
 		}
 
-		// regex b("(.)*&( )*\n");
-		// printf("%d ",backFlag);
-
 		if(fork()==0){
 			executePipe(args, i);
-			// kill(getpid(),SIGTERM);
 		}
 		else {
-			// printf("%d ",backFlag);
-			// usleep(90000);
+			//waiting for child process to exit if backflag is 0	
 			if(backFlag==0){
-				// printf("backflag");
 				while(waitpid(-1,NULL,0)>0);
 			}
 		}
